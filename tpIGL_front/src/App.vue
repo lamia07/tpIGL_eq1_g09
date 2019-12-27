c989d30b7e5c0feabab91596ebc77601ccf36900
lamia07 <hl_kessi@esi.dz>
2019-12-28 00:26:40 +0100

    synchronisation du projet front avec back(le projet front en vuejs est dans le projet back car un repository peut contient un suel projet mais l'integration est faite dans 2 projets differents utilisant les api)

---
+++ tpIGL_front/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app dark app >
+    <div class="appp">
+
+      <div class="container">
+
+        <Authent/>
+      </div><!-- /.container -->
+
+    </div>
+  </v-app>
+</template>
+
+<script>
+import Authent from './components/Authent.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Authent,
+  },
+
+  data: () => ({
+     menu: [
+        { icon: 'home', title: 'Link A' },
+        { icon: 'info', title: 'Link B' },
+        { icon: 'warning', title: 'Link C' }
+      ]
+
+  }),
+};
+</script>
+<style scoped>
+.appp{
+ background-image: url(./img/bureau.jpg);
+ background-attachment: fixed;
+ opacity: 0.8;
+}
+ .container{
+   margin: 0px 0px 0px 0px;
+ }
+</style>
